e0c792b22d1961cc99870bc63bb20608e0e62092
HaDuve <hionoe@hotmail.de>
2019-03-10 14:24:06 +0100

    stats
    created skeleton code for adjustable stats

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ pygame.display.set_caption("SpaceFighters")
 pygame.display.set_icon(pygame.image.load('resources/ship1.png'))
 fpsClock = pygame.time.Clock()
 pygame.mouse.set_visible(0)
+pygame.key.set_repeat(100, 100)
+CURSOR_ROW = 1
+CURSOR   = pygame.image.load('resources/cursor.png')
 
 #CONSTANTS
 
@@ -38,6 +41,68 @@ BLUE =(100,115,175)
 RED =(176,52,52)
 GREEN =(84,155,70)
 
+#STATS to be modified and imported
+#[ShipSpeed, Maneuverability, RocketSpeed, #open]
+STATS1 = [0,0,0,0]
+STATS2 = [0,0,0,0]
+STATS3 = [0,0,0,0]
+STATS4 = [0,0,0,0]
+
+
+def choose_mode(playercount, player = 1):
+    while 1:
+        global CURSOR_ROW
+        DISPLAY.fill(BLACK)
+        DISPLAY.blit(BACKGROUND,(0,0))
+
+        if player == 1:
+            DISPLAY.blit(SHIP1,(180,550))
+        if player == 2:
+            DISPLAY.blit(SHIP2,(180,550))
+        if player == 3:
+            DISPLAY.blit(SHIP3,(180,550))
+        if player == 4:
+            DISPLAY.blit(SHIP4,(180,550))
+
+
+        DISPLAY.blit(CURSOR,(120,CURSOR_ROW*SCREENHEIGHT/15+100))
+        
+        for event in pygame.event.get():
+            if not hasattr(event, 'key'): continue
+                    #QUIT Event
+            elif event.type == KEYDOWN:
+               #print(event.key)
+                if event.key == K_ESCAPE:
+                    main()
+                if event.key == K_BACKSPACE:
+                    if player > 1:
+                        choose_mode(playercount, player - 1)
+                    else:
+                        main()
+                #EDIT STATS WITH RIGHT/LEFT , CHANGE ROW WITH UP/DOWN
+                if event.key == K_UP:
+                    CURSOR_ROW += -1
+                    if CURSOR_ROW < 1:
+                        CURSOR_ROW = 1
+                if event.key == K_DOWN:
+                    CURSOR_ROW += 1
+                    if CURSOR_ROW > 10:
+                        CURSOR_ROW = 10
+
+                if event.key == K_RETURN:
+                    if player == playercount:                        
+                        if playercount == 2:
+                            main2players.main()
+                        if playercount == 3:
+                            main3players.main()
+                        if playercount == 4:
+                            main4players.main()
+                    else:
+                        choose_mode(playercount, player + 1)
+
+        pygame.display.update()
+        fpsClock.tick(60)
+        
 def main():
     
     #MAINLOOP
@@ -85,13 +150,17 @@ def main():
                     pygame.quit()
                     sys.exit()
 
-                # MOVEMENT with ARROW KEYS
+                # Choose Players
+                
                 if (event.key == K_2):
-                    main2players.main()
+                    choose_mode(2)
+                    
                 if (event.key == K_3):
-                    main3players.main()
+                    choose_mode(3)
+                                        
                 if (event.key == K_4):
-                    main4players.main()
+                    choose_mode(4)
+                    
 
 
 
