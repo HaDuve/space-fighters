ffad3bf74ca5c0752953bee2c8824deff90549e9
HaDuve <48185176+HaDuve@users.noreply.github.com>
2019-03-20 13:00:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
-from distutils.core import setup
-import py2exe
+import cx_Freeze
 
-setup(console=['main.py'])
+executables = [cx_Freeze.Executable("main.py")]
+
+cx_Freeze.setup(
+    name="Space fighters",
+    options={"build_exe": {"packages": ["pygame","sys"],
+        "include_files":["main4players.py", "resources/background.png", 'resources/ship1.png',
+                          'resources/ship2.png','resources/ship3.png',
+                          'resources/ship4.png', 'resources/rocket1.png',
+                          'resources/rocket2.png', 'resources/rocket3.png',
+                          'resources/rocket4.png', 'resources/endgamescreen1600.png',
+                          'resources/lukas_powerup.png', 'resources/explosion.png',
+                          'resources/ship1_lightspeed.png', 'resources/spacemine.png',
+                          'resources/ship1_phantom.png', 'resources/ship2_lightspeed.png',
+                          'resources/spacemine.png', 'resources/ship2_phantom.png',
+                          'resources/ship3_lightspeed.png', 'resources/spacemine.png',
+                          'resources/ship3_phantom.png', 'resources/ship4_lightspeed.png',
+                          'resources/spacemine.png', 'resources/ship4_phantom.png'
+                          ]}},
+    executables=executables
+)
